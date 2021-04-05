157b19dac435cf71b4c97a91277d4953dfd72ce4
Code Sandbox <hello@codesandbox.io>
2021-04-05 14:39:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.14ef238fb4ecddc0d87880c3284286cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b7929a62ca2dfd24a1611519304a39",
+    "url": "https://meghna-nakhate.github.io/csb-ff45u/index.html"
+  },
+  {
+    "revision": "4c9ad41c6ba9358fb9ca",
+    "url": "https://meghna-nakhate.github.io/csb-ff45u/static/css/main.c41ee3b4.chunk.css"
+  },
+  {
+    "revision": "d1e032bc3fab171ebb40",
+    "url": "https://meghna-nakhate.github.io/csb-ff45u/static/js/2.91ea96dd.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://meghna-nakhate.github.io/csb-ff45u/static/js/2.91ea96dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9ad41c6ba9358fb9ca",
+    "url": "https://meghna-nakhate.github.io/csb-ff45u/static/js/main.26a374fc.chunk.js"
+  },
+  {
+    "revision": "4c87070f11d9a911b2b3",
+    "url": "https://meghna-nakhate.github.io/csb-ff45u/static/js/runtime-main.bee3cc63.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://meghna-nakhate.github.io/csb-ff45u/precache-manifest.14ef238fb4ecddc0d87880c3284286cf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://meghna-nakhate.github.io/csb-ff45u/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
